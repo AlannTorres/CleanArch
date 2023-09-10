f30e1c07944c73dbfe68300da4f8f475309e1dac
AlannTorres <alanvictor.torres@gmail.com>
2023-09-10 16:09:49 -0300

    feat: Criação da camada de Application, criando os DTOs e Services

---
+++ CleanArchMvc.Application/Services/CategoryServices.cs
@@ -0,0 +1,45 @@
+﻿using AutoMapper;
+using CleanArchMvc.Application.DTOs;
+using CleanArchMvc.Application.Interfaces;
+using CleanArchMvc.Domain.Interfaces;
+
+namespace CleanArchMvc.Application.Services;
+
+public class CategoryServices : ICategoryService
+{
+    private readonly ICategoryRepository _categoryRepository;
+    private readonly IMapper _mapper;
+
+    public CategoryServices(ICategoryRepository categoryRepository, IMapper mapper)
+    {
+        _categoryRepository = categoryRepository;
+        _mapper = mapper;
+    }
+
+    public async Task<IEnumerable<CategoryDTO>> GetCategories()
+    {
+        var categoriesEntity = await _categoryRepository.GetCategoriesAsync();
+        return _mapper.Map<IEnumerable<CategoryDTO>>(categoriesEntity);
+    }
+
+    public async Task<CategoryDTO> GetById(int id)
+    {
+        var categoryEntity = await _categoryRepository.GetByIdAsync(id);
+        return _mapper.Map<CategoryDTO>(categoryEntity);
+    }
+
+    public async Task Add(CategoryDTO categoryDTO)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task Remove(int? id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task Update(CategoryDTO categoryDTO)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ CleanArchMvc.Application/Services/ProductService.cs
@@ -0,0 +1,54 @@
+﻿using AutoMapper;
+using CleanArchMvc.Application.DTOs;
+using CleanArchMvc.Application.Interfaces;
+using CleanArchMvc.Domain.Entities;
+using CleanArchMvc.Domain.Interfaces;
+
+namespace CleanArchMvc.Application.Services;
+
+public class ProductService : IProductService
+{
+    private readonly IProductRespository _productRespository;
+    private readonly IMapper _mapper;
+
+    public ProductService(IProductRespository productRespository, IMapper mapper)
+    {
+        _productRespository = productRespository;
+        _mapper = mapper;
+    }
+    public async Task<ProductDTO> GetProductCategory(int id)
+    {
+        var productEntity = await _productRespository.GetProductCategoryAsync(id);
+        return _mapper.Map<ProductDTO>(productEntity);
+    }
+
+    public async Task<IEnumerable<ProductDTO>> GetProducts()
+    {
+        var productsEntity = await _productRespository.GetProductsAsync();
+        return _mapper.Map<IEnumerable<ProductDTO>>(productsEntity);
+    }
+
+    public async Task<ProductDTO> GetById(int id)
+    {
+        var productEntity = await _productRespository.GetByIdAsync(id);
+        return _mapper.Map<ProductDTO>(productEntity);
+    }
+
+    public async Task Add(ProductDTO productDTO)
+    {
+        var productEntity = _mapper.Map<Product>(productDTO);
+        await _productRespository.CreateAsync(productEntity);
+    }
+
+    public async Task Update(ProductDTO productDTO)
+    {
+        var productEntity = _mapper.Map<Product>(productDTO);
+        await _productRespository.UpdateAsync(productEntity);
+    }
+
+    public async Task Delete(int id)
+    {
+        var productEntity = _productRespository.GetByIdAsync(id).Result;
+        await _productRespository.RemoveAsync(productEntity);
+    }
+}
